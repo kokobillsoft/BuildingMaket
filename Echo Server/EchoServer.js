50283d9e151590a105d103b66d07476cbef611ee
kokobill <kokobillsoft@gmail.com>
2018-04-03 15:55:48 +0300

    EchoServer

---
+++ Echo Server/EchoServer.js
@@ -0,0 +1,21 @@
+
+// http://127.0.0.1/echo?message=Hello -> Hello
+ 
+var http = require('http');
+var url = require('url');
+ 
+var server = new http.Server(function(req, res){
+    console.log ( req.method, req.url );
+ 
+    var urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+ 
+    if(urlParsed.pathname == '/echo' && urlParsed.query.message){
+        res.end("Blyat");
+    }else{
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+});
+ 
+server.listen(1337, '127.0.0.1');
