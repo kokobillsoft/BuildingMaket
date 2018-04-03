8d2c85fffdbed13fa2f1be5233af87cd49c49a03
kokobill <kokobillsoft@gmail.com>
2018-04-03 16:21:16 +0300

    Parsing_Get

--- Echo Server/EchoServer.js
+++ Echo Server/EchoServer.js
@@ -4,14 +4,20 @@
 var http = require('http');
 var url = require('url');
  
-var server = new http.Server(function(req, res){
-    console.log ( req.method, req.url );
+var server = new http.Server(function(req, res){ // запуск сервера
+    console.log ( req.method, req.url );         // вывод в консоль метода и тела запроса
  
-    var urlParsed = url.parse(req.url, true);
-    console.log(urlParsed);
+    var urlParsed = url.parse(req.url, true);    // парсинг URL 
+    console.log(urlParsed);                       
  
     if(urlParsed.pathname == '/echo' && urlParsed.query.message){
-        res.end("Blyat");
+        if(urlParsed.query.message == 'Test1'){
+            res.end("Test1 command found");
+        }
+        if(urlParsed.query.message == 'test2'){
+            res.end("test2 command found");
+        }
+        res.end("Bad command");
     }else{
         res.statusCode = 404;
         res.end("Page not found");
