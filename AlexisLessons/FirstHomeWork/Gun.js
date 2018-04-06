935ecaff89659cfe9e309c8325e31d45f1c84b8f
kokobill <kokobillsoft@gmail.com>
2018-04-06 18:32:21 +0300

    Gun (light version)

--- AlexisLessons/FirstHomeWork/Gun.js
+++ AlexisLessons/FirstHomeWork/Gun.js
@@ -3,25 +3,16 @@ function main(){                                //main of programm
     var maxAmmoGun1 = 9;                        // Set maxAmmo in clipp
     var gun1 = new HandGun(maxAmmoGun1);        // Create new object
                      
-    console.log(gun1);                          // Print oject to console
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
+    console.log(gun1);       
+    for (i=0; i<4; i++){
+        console.log(gun1.shot()); 
+    }                   
     console.log(gun1);                          // Print oject to console                   
     console.log(gun1.reload(2));                // charge the clip with two ammo        
     console.log(gun1.reload(4));                // charge the clip with four ammo
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
-    console.log(gun1.shot());                   // Do a shot
+    for (i=0; i<11; i++){
+        console.log(gun1.shot()); 
+    }
     console.log(gun1.reload(10));               // charge the clip with ten ammo
     console.log(gun1.shot());                   // Do a shot
 }
@@ -39,23 +30,21 @@ class HandGun {                                 // description of new class
             return('There was a shot. Ammo in Gun ' + this.ammoInGun); // return Notification
         }
     }
-    reload(n){                                   // function of reload gun
-        if (this.ammoInGun == 0){                // Check the presence of ammo in barrel. If ammo unavailable
-            var unnecessaryAmmo = this.ammoInGun + n - this.maxAmmo; // Create variable of unnecessary ammo
-        }else{                                   // If ammo available in barrel
-            var unnecessaryAmmo = this.ammoInGun + n -1 - this.maxAmmo; // Create variable of unnecessary ammo
-        }
-        if (unnecessaryAmmo > 0 && this.ammoInGun != 0){ //Check if we can completely clip and barrel of ammo
-            this.ammoInGun = this.maxAmmo + 1;    // perform reload of ammo
-            return ("I can`t charge so much. Your clip is full. you hawe annecessary " + unnecessaryAmmo + " ammo" ); // return Notification
-        }else if (unnecessaryAmmo > 0 && this.ammoInGun == 0){  //Check if we can completely clip ammo without ammo in barrel 
-                this.ammoInGun = this.maxAmmo;    // perform reload of ammo
-                return ("I can`t charge so much. Your clip is full. you hawe annecessary " + (unnecessaryAmmo + 1) + " ammo" ); // return Notification
-                }else{
-                    this.ammoInGun = this.ammoInGun + n;  // perform reload of ammo
-                    return ("Reload completed. Yo hawe a " + this.ammoInGun + " ammo"); // return Notification
+    reload(n){
+        if (n>0){
+            var AmmoInBuller = 0;
+            if (this.ammoInGun>0){
+                AmmoInBuller = 1;
+            }
+            if((this.ammoInGun + n) > (this.maxAmmo + AmmoInBuller)){
+                this.ammoInGun = this.maxAmmo + AmmoInBuller;
+                return('Reload done, You have unnecessary ' + (n - this.maxAmmo - AmmoInBuller) + ' ammo');
+            }
+            this.ammoInGun += n + AmmoInBuller;
+            return('Reload done');
         }
     }
 }
+
 
 main();
